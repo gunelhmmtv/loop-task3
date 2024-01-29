b901ab00431a1db80644cc695c64c5945bffdf96
gunelhmmtv <gunell.hummetovaa@gmail.com>
2024-01-30 01:31:52 +0400

    loop tas3

---
+++ Loop task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 304001;
+int sum = 0;
+int count = 0;  
+
+
+while (number>0)
+{
+
+    int say = number % 10;
+    if (say > 0)
+    {
+        sum += say;
+    }
+    else
+    { 
+        count++;
+    }
+    number /= 10;
+}
+Console.WriteLine($"Musbet reqemlerin cemi= {sum}");
+Console.WriteLine($"Musbet ve menfi olmayan ededlerin sayi={count}");
